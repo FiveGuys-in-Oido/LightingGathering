0f7bbc1c7296c9682789729ac53e746f68344653
GaBaljaintheroom <junsu1222@naver.com>
2023-05-29 17:15:34 +0900

    feat: 검색 페이지 완성

--- app/src/main/java/com/tuk/lightninggathering/PostAdapter.java
+++ app/src/main/java/com/tuk/lightninggathering/PostAdapter.java
@@ -1,5 +1,7 @@
 package com.tuk.lightninggathering;
 
+import android.content.Context;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +16,20 @@ import java.util.List;
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
 
     private List<Post> postList;
+    private Context context;
 
-    public PostAdapter(List<Post> postList) {
+    public PostAdapter(List<Post> postList, Context context) {
         this.postList = postList;
+        this.context = context;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_post, parent, false);
+        RecyclerView.LayoutParams layoutParams = (RecyclerView.LayoutParams) view.getLayoutParams();
+        layoutParams.setMargins(0, dpToPx(10), 0, dpToPx(10)); // 상단과 하단에 10dp 간격을 설정합니다.
+        view.setLayoutParams(layoutParams);
         return new ViewHolder(view);
     }
 
@@ -54,5 +61,9 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
             locationTextView = itemView.findViewById(R.id.locationTextView);
             participantsTextView = itemView.findViewById(R.id.participantsTextView);
         }
+    }
+
+    private int dpToPx(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
     }
 }
--- app/src/main/java/com/tuk/lightninggathering/SearchActivity.kt
+++ app/src/main/java/com/tuk/lightninggathering/SearchActivity.kt
@@ -23,9 +23,10 @@ class SearchActivity : AppCompatActivity() {
         postList.add(Post("게시물 제목 1", "2023-05-30", "장소 1", "참여인원/현재인원 1"))
         postList.add(Post("게시물 제목 2", "2023-05-31", "장소 2", "참여인원/현재인원 2"))
         postList.add(Post("게시물 제목 3", "2023-06-01", "장소 3", "참여인원/현재인원 3"))
+        postList.add(Post("게시물 제목 4", "2023-06-02", "장소 4", "참여인원/현재인원 4"))
 
         // 어댑터 설정
-        postAdapter = PostAdapter(postList)
+        postAdapter = PostAdapter(postList, this)
         recyclerView.adapter = postAdapter
     }
 }
