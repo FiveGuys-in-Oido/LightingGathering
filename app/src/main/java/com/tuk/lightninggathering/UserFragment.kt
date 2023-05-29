25a3c11d92a7817b6733b056b2357d3afdc12693
JunRain <sa2313@tukorea.ac.kr>
2023-05-29 22:23:26 +0900

    MapActivity에서 데이터를 전달받는 것 구현

--- app/src/main/java/com/tuk/lightninggathering/UserFragment.kt
+++ app/src/main/java/com/tuk/lightninggathering/UserFragment.kt
@@ -1,11 +1,14 @@
 package com.tuk.lightninggathering
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.model.LatLng
 import com.tuk.lightninggathering.databinding.FragmentUserBinding
 
 class UserFragment : Fragment() {
@@ -26,6 +29,7 @@ class UserFragment : Fragment() {
         binding.btnChangeAddress.setOnClickListener {
             val intent = Intent(activity, MapActivity::class.java)
             startActivityForResult(intent, REQUEST_CODE_MAP)
+
         }
 
         return view
@@ -39,4 +43,19 @@ class UserFragment : Fragment() {
     companion object {
         private const val REQUEST_CODE_MAP = 100
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_MAP && resultCode == Activity.RESULT_OK) {
+            val markerLocation = data?.getParcelableExtra<LatLng>("marker_location")
+            if (markerLocation != null) {
+                // 받아온 데이터를 회원 정보에 입력을 구현해야함
+                Log.i("position","$markerLocation")
+            }
+        }
+    }
+
+
 }
+
