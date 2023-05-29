9d838b57863e9ea07feb964debb17bfadaf57406
JeongHunHui <deca3248@gmail.com>
2023-05-29 22:44:42 +0900

    feat: 모임 상세 정보 더미데이터로 보여주는 기능 추가, 뒤로가기 버튼 구현

--- app/src/main/java/com/tuk/lightninggathering/GatheringDetailActivity.kt
+++ app/src/main/java/com/tuk/lightninggathering/GatheringDetailActivity.kt
@@ -1,40 +1,50 @@
 package com.tuk.lightninggathering
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.google.android.material.tabs.TabItem
 
 class GatheringDetailActivity : AppCompatActivity() {
 
-    // ViewPager를 위한 Adapter
+    private lateinit var title: String
+    private lateinit var memo: String
+    private lateinit var date: String
+    private lateinit var placeName: String
+    private lateinit var placeAddress: String
+    private lateinit var category: String
+    private var currentPeople: Int = 4
+    private var maxPeople: Int = 8
+    private var latitude: Double = 0.0
+    private var longitude: Double = 0.0
+
     inner class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
         override fun getItem(position: Int): Fragment {
             return when (position) {
                 0 -> {
-                    // DetailsFragment에 전달할 데이터를 Bundle에 넣습니다.
                     val bundle = Bundle().apply {
-                        // 아래의 "memo", "date" 등은 예시이므로 실제 앱에 필요한 데이터로 바꾸어주세요.
-                        putString("memo", "example memo")
-                        putString("date", "example date")
-                        putString("placeName", "example placeName")
-                        putString("placeAddress", "example placeAddress")
-                        putDouble("latitude", 0.0)
-                        putDouble("longitude", 0.0)
+                        putString("memo", memo)
+                        putString("date", date)
+                        putString("placeName", placeName)
+                        putString("placeAddress", placeAddress)
+                        putString("category", category)
+                        putDouble("latitude", latitude)
+                        putDouble("longitude", longitude)
                     }
 
-                    // DetailsFragment를 생성하고 Bundle을 설정합니다.
                     DetailsInfoFragment().apply {
                         arguments = bundle
                     }
                 }
-                1 -> MemberFragment() // MemberFragment를 반환합니다.
-                2 -> ReviewFragment() // ReviewFragment를 반환합니다.
+                1 -> MemberFragment()
+                2 -> ReviewFragment()
                 else -> throw IllegalStateException("Unexpected position: $position")
             }
         }
@@ -45,6 +55,26 @@ class GatheringDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gathering_detail)
+        val showTitle: TextView = findViewById(R.id.showTitle);
+        val showPeopleNum: TextView = findViewById(R.id.showPeoplenum);
+        val toolbar: Toolbar? = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        title = "롤 5:5 하실분 구합니다!" // intent.getStringExtra("title")!!
+        memo = "실버 이상, 플래티넘 이하 분들로 구합니다!" // intent.getStringExtra("memo")!!
+        date = "2023년 6월 1일" // intent.getStringExtra("date")!!
+        placeName = "정왕동 보보스 PC방" // intent.getStringExtra("placeName")!!
+        placeAddress = "정왕동 1302-5" // intent.getStringExtra("placeAddress")!!
+        category = "리그오브레전드" // intent.getStringExtra("category")!!
+        currentPeople = 6 // intent.getIntExtra("currentPeople")!!
+        maxPeople = 10 // intent.getIntExtra("maxPeople")!!
+        latitude = 5.0 // intent.getDoubleExtra("latitude", 0.0)
+        longitude = 5.0 // intent.getDoubleExtra("longitude", 0.0)
+
+        showTitle.text = title
+        showPeopleNum.text = "인원: ${currentPeople} / ${maxPeople}"
 
         val sectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
         val viewPager: ViewPager = findViewById(R.id.container)
@@ -55,5 +85,15 @@ class GatheringDetailActivity : AppCompatActivity() {
         tabs.getTabAt(0)?.text = getString(R.string.details)
         tabs.getTabAt(1)?.text = getString(R.string.member)
         tabs.getTabAt(2)?.text = getString(R.string.reviews)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
