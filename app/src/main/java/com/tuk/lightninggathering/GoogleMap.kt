df1710b920646581f5028376e5eff11e82abc78a
JunRain <sa2313@tukorea.ac.kr>
2023-05-29 15:34:22 +0900

    MyPage,Map 레이아웃 생성

--- app/src/main/java/com/tuk/lightninggathering/GoogleMap.kt
+++ app/src/main/java/com/tuk/lightninggathering/GoogleMap.kt
@@ -3,8 +3,7 @@ package com.tuk.lightninggathering
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-
-class MainActivity : AppCompatActivity() {
+class GoogleMap : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.map)
