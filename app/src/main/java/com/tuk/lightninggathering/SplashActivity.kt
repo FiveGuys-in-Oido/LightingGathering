139d04f6283b1d3f87cda9a4eb0f2f0e3643645e
LeeJiWon <dlwldnjs1009@gmail.com>
2023-05-30 12:24:16 +0900

    feat: 인트로 애니메이션 구현
    feat: 인트로 애니메이션 구현

---
+++ app/src/main/java/com/tuk/lightninggathering/SplashActivity.kt
@@ -0,0 +1,43 @@
+package com.tuk.lightninggathering
+
+import android.animation.Animator
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieDrawable
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.tuk.lightninggathering.R.layout.activity_splash)
+
+        val animationView = findViewById<LottieAnimationView>(R.id.one_btn).apply {
+            speed = 1f
+            setAnimation("111632-lightningneon.json") // replace this with your animation file
+            enableMergePathsForKitKatAndAbove(true)
+            visibility = View.VISIBLE
+            repeatCount = LottieDrawable.INFINITE
+            addAnimatorListener(object : Animator.AnimatorListener {
+                override fun onAnimationStart(animation: Animator) {}
+                override fun onAnimationEnd(animation: Animator) { visibility = View.GONE }
+                override fun onAnimationCancel(animation: Animator) {}
+                override fun onAnimationRepeat(animation: Animator) {}
+            })
+            playAnimation()
+        }
+
+        Handler().postDelayed({
+            val intent = Intent(this, NaviActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 6000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
+    }
+}
