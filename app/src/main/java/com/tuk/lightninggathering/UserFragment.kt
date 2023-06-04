657048744e26c198fe0846b14df944a6d115c7e6
LeeJiWon <dlwldnjs1009@gmail.com>
2023-06-04 22:02:20 +0900

    [feat] 로그아웃 구현
    [feat] 로그아웃 구현

--- app/src/main/java/com/tuk/lightninggathering/UserFragment.kt
+++ app/src/main/java/com/tuk/lightninggathering/UserFragment.kt
@@ -7,13 +7,22 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.maps.model.LatLng
+import com.google.firebase.auth.FirebaseAuth
 import com.tuk.lightninggathering.databinding.FragmentUserBinding
 
 class UserFragment : Fragment() {
     private var _binding: FragmentUserBinding? = null
     private val binding get() = _binding!!
+
+    // 로그아웃 구현을 위한 변수
+    var auth : FirebaseAuth ?= null
+    var googleSignInClient : GoogleSignInClient ?= null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,6 +53,29 @@ class UserFragment : Fragment() {
             startActivity(intent)
         }
 
+        // 구글 로그아웃을 위해 로그인 세션 가져오기
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.firebase_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        // firebaseauth를 사용하기 위한 인스턴스 get
+        auth = FirebaseAuth.getInstance()
+
+        // 구글 로그아웃 버튼 ID 가져오기
+        var google_sign_out_button = view.findViewById<Button>(R.id.google_sign_out_button)
+
+        // 구글 로그아웃 버튼 클릭 시 이벤트
+        google_sign_out_button.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            googleSignInClient?.signOut()
+
+            var logoutIntent = Intent (requireActivity(), LoginActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(logoutIntent)
+        }
+
         return view
     }
 
@@ -67,7 +99,4 @@ class UserFragment : Fragment() {
             }
         }
     }
-
-
 }
-
